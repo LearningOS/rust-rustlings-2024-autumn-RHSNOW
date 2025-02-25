3146dd43d4567d5d7a27d07d128592cb63d20e74
RSNOW <583041590@qq.com>
2025-02-25 03:26:32 +0000

    upload once

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,31 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+   input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    //???
+    let a = " world!";
+    let mut input = String::from(input);
+    input.push_str(a);
+    input
+
+//----
+    // input.to_string() + " world!"
+//----
+    // format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    //???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
